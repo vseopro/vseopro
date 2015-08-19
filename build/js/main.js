bac55eafde7babfd39694bfee1ab94083b1aef05
Alexander Anpleenko <vaeum@yahoo.com>
2015-08-19 20:40:56 +0300

    fix

--- build/js/main.js
+++ build/js/main.js
@@ -1,16 +1,17 @@
 (function() {
-  $('.open-menu__button').on('click', function() {
-    var targetAction;
-    targetAction = $(this).data('show');
-    if (targetAction === 'left-menu') {
-      return $('html').toggleClass('open-left-mobile-menu');
-    } else {
-      return $('html').toggleClass('open-right-mobile-menu');
-    }
-  });
-
-  $('.mobile-overlay').on('click', function() {
-    return $('html').attr("class", '');
+  $(function() {
+    $('.open-menu__button').on('click', function() {
+      var targetAction;
+      targetAction = $(this).data('show');
+      if (targetAction === 'left-menu') {
+        return $('html').toggleClass('open-left-mobile-menu');
+      } else {
+        return $('html').toggleClass('open-right-mobile-menu');
+      }
+    });
+    return $('.mobile-overlay').on('click', function() {
+      return $('html').attr("class", '');
+    });
   });
 
 }).call(this);
--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,10 +1,11 @@
-$('.open-menu__button').on 'click', ->
-  targetAction = $(@).data('show')
-  if targetAction == 'left-menu'
-    $('html').toggleClass('open-left-mobile-menu')
-  else
-    $('html').toggleClass('open-right-mobile-menu')
+$ ->
+  $('.open-menu__button').on 'click', ->
+    targetAction = $(@).data('show')
+    if targetAction == 'left-menu'
+      $('html').toggleClass('open-left-mobile-menu')
+    else
+      $('html').toggleClass('open-right-mobile-menu')
 
-$('.mobile-overlay').on 'click', ->
-  $('html').attr("class", '')
+  $('.mobile-overlay').on 'click', ->
+    $('html').attr("class", '')
 
